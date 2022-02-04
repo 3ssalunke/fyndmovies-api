5e1e9bb805fcf706c9ac0829e5a71815c00e5874
surajs <3ssalunke>
2022-02-04 12:53:28 +0530

    experiment with babel config

--- src/server/startServer.js
+++ src/server/startServer.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
-//all routes are in routes file
+//all routes are in one file for now
 setUpRoutes(app);
 //error middleware
 app.use((err, req, res, next) => {

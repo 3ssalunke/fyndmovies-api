ddcc93fac125fa3934a1a5ead0f6d56d82a97710
surajs <3ssalunke>
2022-02-05 00:24:03 +0530

    response format refactored

--- src/helpers/tokenValidator.js
+++ src/helpers/tokenValidator.js
@@ -4,13 +4,17 @@ const validate = (req, res, next) => {
   //check for authorization header
   const authHeader = req.headers.authorization?.split(" ")[1];
   if (!authHeader) {
-    return res.status(401).json({ message: "authorization header required" });
+    return res
+      .status(401)
+      .json({ status: "FAILURE", message: "authorization header required" });
   }
   //decoding token
   const decodedToken = decode(authHeader);
   //if token is not valid
   if (decodedToken === "" || decodedToken === "invalid token") {
-    return res.status(403).json({ message: "invalid token or token expired" });
+    return res
+      .status(403)
+      .json({ status: "FAILURE", message: "invalid token or token expired" });
   }
   next();
 };
